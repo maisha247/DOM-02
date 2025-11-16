9bf61cbad3d687ff18c9aca3c8000bafc3e665f5
Maisha Tasnim Mridula <maishatasnimmridula@gmail.com>
2025-11-17 00:22:47 +0600

    DOM-02

--- js/script.js
+++ js/script.js
@@ -1,13 +1,16 @@
-let reviewContainer = document.getElementById("reviewContainer")
-let reviewInput = document.getElementById("reviewInput")
-let addReviewBtn = document.getElementById("addReviewBtn")
+// =======================================================================
+// let reviewContainer = document.getElementById("reviewContainer")
+// let reviewInput = document.getElementById("reviewInput")
+// let addReviewBtn = document.getElementById("addReviewBtn")
 
-addReviewBtn.addEventListener("click", ()=> {
-    let inputValue = reviewInput.value 
-    // console.log(inputValue);
-    let paragraph = document.createElement("p")
-    paragraph.innerHTML= inputValue
-    reviewContainer.appendChild(paragraph)
-    reviewInput.value=""
-    // =======================    
-})
+// addReviewBtn.addEventListener("click", ()=> {
+//     let inputValue = reviewInput.value 
+//     // console.log(inputValue);
+//     let paragraph = document.createElement("p")
+//     paragraph.innerHTML= inputValue
+//     reviewContainer.appendChild(paragraph)
+//     reviewInput.value=""   
+// })
+// =======================================================================
+
+
